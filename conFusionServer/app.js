314cbc037462b781aca6aee7ce5285dc0da6bc6e
msksaul <64514098+msksaul@users.noreply.github.com>
2020-09-01 22:32:00 -0600

    Express Sessions Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -39,45 +39,26 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session)
 
   if (!req.session.user) {
-    let authHeader = req.headers.authorization
+    let err = new Error('You are not authenticated!')
 
-    if (!authHeader) {
-      let err = new Error('You are not authenticated!')
-
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
-
-    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-
-    let username = auth[0]
-    let password = auth[1]
-
-    if (username === 'admin' && password === 'password') {
-      req.session.user = 'admin'
-      next()
-    }
-    else {
-      let err = new Error('You are not authenticated!')
-
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
+    err.status = 401
+    return next(err)
   }
   else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next()
     }
     else {
       let err = new Error('You are not authenticated!')
 
-      err.status = 401
+      err.status = 403
       return next(err)
     }
   }
@@ -87,8 +68,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
